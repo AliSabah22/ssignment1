f792980cb2488f1d117fb43f38ce30e972d1fc4f
AliSabah22 <aliasabah11@gmail.com>
2026-02-11 12:04:04 -0500

    home page finished, random number generation functionality is finished, and page navigations is done

--- lib/main.dart
+++ lib/main.dart
@@ -16,21 +16,39 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       
       
-      title: 'Flutter Demo',
+      
+      title: 'Random Number Generator',
       theme: ThemeData(
-        appBarTheme: AppBarThemeData(
-          backgroundColor: const Color(0xFF147CD3)
+        scaffoldBackgroundColor: const Color(0xFF2196F3),
+        
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFF147CD3),
+          foregroundColor: Colors.white,
         ),
-        primaryColor:Color(0xFF2196F3),
+
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: Color(0xFF147CD3),
+          foregroundColor: Colors.white
+        ),
+
+
         elevatedButtonTheme: ElevatedButtonThemeData(
          style: ElevatedButton.styleFrom(
-          backgroundColor: const Color(0xFF147CD3)
-         )
+          backgroundColor: const Color(0xFF147CD3),
+          foregroundColor: Colors.white,
+          elevation: 5,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+         ),
         ),  
 
         textTheme: const TextTheme(
-          
+          bodyLarge: TextStyle(color: Colors.white),
+          bodyMedium: TextStyle(color: Colors.white),
+          titleLarge: TextStyle(color: Colors.white),
         ),
+        
         
        
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
@@ -51,6 +69,65 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int? lastNumber;
+
+  void generateRandomNumber() {
+    setState(() {
+      lastNumber = Random().nextInt(9) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.home),
+        title: Text(widget.title),
+        backgroundColor: Color(0xFF147CD3),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              lastNumber?.toString() ?? "",
+            ),
+            const SizedBox(height: 30,),
+            ElevatedButton(
+              onPressed: generateRandomNumber,
+              child: const Text("Generate"),
+            ),
+
+            SizedBox(height: 20),
+
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => const StatisticsPage(),
+                    )
+                  );
+              },
+              child: const Text("View Statistics"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//Statistics page
+class StatisticsPage extends StatefulWidget {
+  const StatisticsPage({super.key,});
+
+
+  @override
+  State<StatisticsPage> createState() => _StatisticsPage();
+}
+
+class _StatisticsPage extends State<StatisticsPage> {
   int _counter = 0;
 
   void _incrementCounter() {
@@ -59,88 +136,26 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        backgroundColor: Color(0xFF147CD3),
-      ),
-      body: Center(
-        child: Text(
-          'Counter: $_counter',
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
 
-
-// class StatisticsPage extends StatefulWidget {
-//   const StatisticsPage({super.key, required this.title});
-
-//   final String title;
-
-//   @override
-//   State<StatisticsPage> createState() => _StatisticsPage();
-// }
-
-// class _StatisticsPage extends State<StatisticsPage> {
-  // int _counter = 0;
-
-  // void _incrementCounter() {
-  //   setState(() {
-  //     _counter++;
-  //   });
-  // }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         child: Text(
-//           // 'Stats Counter: $_counter',
-//           // style: TextStyle(fontSize: 24),
-//         ,)
-//       ),
-//     );
-//   }
-// }
  
 
 
-  // @override
-  // Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
 
-  //   return Scaffold(
-  //     appBar: AppBar(
-  //       backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-  //       title: Text(widget.title),
-  //     ),
-  //     body: Center(
-  //       child: Column(
-  //         mainAxisAlignment: .center,
-  //         children: [
-  //           const Text('You have pushed the button this many times:'),
-  //           Text(
-  //             '$_counter',
-  //             style: Theme.of(context).textTheme.headlineMedium,
-  //           ),
-  //         ],
-  //       ),
-  //     ),
-  //     floatingActionButton: FloatingActionButton(
-  //       onPressed: _incrementCounter,
-  //       tooltip: 'Increment',
-  //       child: const Icon(Icons.add),
-  //     ),
-  //   );
-  // }
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context),
+          ),
+          title: const Text("Statistics"),
+        ),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        
+      
+      body: const Center(
+        child: Text("Statistics Screen")),
+    );
+  }
+}
